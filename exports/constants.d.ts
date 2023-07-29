7b4c3b11e3cf07341fb63dbc8daec7aada1ad2a0
Glenn Vandeuren <vandeurenglenn@gmail.com>
2023-07-29 21:08:28 +0200

    build

--- exports/constants.d.ts
+++ exports/constants.d.ts
@@ -1,4 +1,3 @@
-import { BaseOptions, MaterialSymbolsVariant, StylingOptions } from "./types.js";
-export declare const variants: MaterialSymbolsVariant[];
+import {BaseOptions, StylingOptions} from './types.js';
 export declare const baseStylingOptions: StylingOptions;
 export declare const baseOptions: BaseOptions;
