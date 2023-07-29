bdf01ddb65bb4a08e7558befee44affff0955c48
vdegenne <vdegenne@gmail.com>
2023-07-29 20:22:47 +0200

    pretty things

--- src/constants.ts
+++ src/constants.ts
@@ -1,21 +1,15 @@
-import { BaseOptions, MaterialSymbolsVariant, StylingOptions } from "./types.js"
-
-// export const variants: MaterialSymbolsVariant[] = [
-//   'outlined',
-//   'rounded',
-//   'sharp'
-// ]
+import {BaseOptions, StylingOptions} from './types.js';
 
 export const baseStylingOptions: StylingOptions = {
   size: 48,
   weight: 400,
   fill: 0,
-  GRAD: 0
-}
+  GRAD: 0,
+};
 
 export const baseOptions: BaseOptions = {
   variant: 'outlined',
   includeHTML: true,
   copyHTML: true,
-  styling: baseStylingOptions
-}
+  styling: baseStylingOptions,
+};
